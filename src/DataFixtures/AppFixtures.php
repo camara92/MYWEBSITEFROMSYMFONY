1e7b6fbf5ef934064df79ba89a3595cbce043d14
camara92 <88945535+camara92@users.noreply.github.com>
2022-05-26 22:26:00 +0200

    fin de section project et creation entité User

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,29 +13,31 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+        //https://picsum.photos/200/300
+        //lien angular : https://github.com/camara92/Maboutique-GP-Angular-Ecommerce-
          for ($i = 0; $i < 5; $i++) {
             $projet = new Projects();
-            // $projet->setProjectname("Ma boutique ecommerce ");
-            // $projet->setContent("Un projet réalisé sous le framework Angular. Le but est de mettre mes compétences dans la pratique.");
-            // $projet->setImage("https://picsum.photos/200/300");
-            // $projet->setCategorie("travail personnel");
-            // $projet->setDatedebut(new DateTime());
-            // $projet->setDatefin(new DateTime());
-            // $projet->setAuthor("Daouda CAMARA"); 
-            // $projet->setTechno("Framework Angular"); 
-            // $projet->setLienGit("https://github.com/camara92/Maboutique-GP-Angular-Ecommerce-");
+            $projet->setProjectname("Ma boutique ecommerce ");
+            $projet->setContent("Un projet réalisé sous le framework Angular. Le but est de mettre mes compétences dans la pratique.");
+            $projet->setImage("/imagesdatafixtures/angular.png");
+            $projet->setCategorie("travail personnel");
+            $projet->setDatedebut(new DateTime());
+            $projet->setDatefin(new DateTime());
+            $projet->setAuthor("Daouda CAMARA"); 
+            $projet->setTechno("Framework Angular"); 
+            $projet->setLienGit("https://github.com/camara92/Maboutique-GP-Angular-Ecommerce-");
             // partie Symfony project en dur 
 
             
-            $projet->setProjectname("YesCode ");
-            $projet->setContent("Un projet réalisé sous le framework Symfony. Le but est de mettre mes compétences dans la pratique.");
-            $projet->setImage("https://picsum.photos/200/300");
-            $projet->setCategorie("travail pratique");
-            $projet->setDatedebut(new DateTime());
-            $projet->setDatefin(new DateTime());
-            $projet->setAuthor("Formation"); 
-            $projet->setTechno("Framework Symfony"); 
-            $projet->setLienGit("https://github.com/camara92/YesCode/");
+            // $projet->setProjectname("YesCode ");
+            // $projet->setContent("Un projet réalisé sous le framework Symfony. Le but est de mettre mes compétences dans la pratique.");
+            // $projet->setImage("https://picsum.photos/200/300");
+            // $projet->setCategorie("travail pratique");
+            // $projet->setDatedebut(new DateTime());
+            // $projet->setDatefin(new DateTime());
+            // $projet->setAuthor("Formation"); 
+            // $projet->setTechno("Framework Symfony"); 
+            // $projet->setLienGit("https://github.com/camara92/YesCode/");
 
 
             $manager->persist($projet);
